9a9f0094d1a0d57bc54276abc480acd950bad3b5
Ullrika <ullrika.sahlin@cec.lu.se>
2023-10-31 13:36:13 +0100

    10 lost prob

--- files/defaultdist.R
+++ files/defaultdist.R
@@ -2,20 +2,20 @@ library(ggplot2)
 library(dplyr)
 
 df <- do.call('rbind',lapply(1:10^4,function(i){
-x = rlnorm(200)
+#x = rlnorm(200)
 #x = rnorm(200)
-#x = rexp(200)
+x = rexp(200)
 
 data.frame(type=c("average","P95","max"), value=c(mean(x),
-quantile(x,probs=0.95),max(x)))
+quantile(x,probs=0.99),max(x)))
 }))
 
-
-ggplot(df,aes(x=value, fill = type)) +
+df %>%
+  #filter(type!="max") %>%
+ggplot(aes(x=value, fill = type)) +
   geom_density(alpha = 0.5)
 
-df %>%
-  filter(type=="P95") %>%
-  select(value) %>%
-  qqnorm()  
+install.packages("sn")
+library(sn)
 
+hist(sn::rsn(10^4,0,1,1))
